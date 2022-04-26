c755dc008fa9ff9272aa9e1bc422165b333c0abe
davikitagawa <davikitagawa@hotmail.com>
2022-04-26 18:19:17 -0300

    - Initial Commit

---
+++ Dev5/Dev5/Program.cs
@@ -0,0 +1,17 @@
+using Dev5.View.Forms;
+
+namespace Dev5
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Home());
+        }
+    }
+}
